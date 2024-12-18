82d54345c6c621c23e12f64e23e1740ea91dfbc0
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 21:38:30 +0200

    Modules in Python. Creating and working with modules

---
+++ modules.py
@@ -0,0 +1,38 @@
+# Модулі у мові Пітон. Створення та робота з модулями
+
+# import time
+# import datetime as date
+# import sys, os, platform
+
+# import cowsay
+
+# cowsay.cow("Hello, Inna!")
+
+# import math
+# from math import sqrt as s, ceil
+
+# time.sleep(2)
+# print("Hello, Inna!")
+
+# print(date.datetime.now().time())
+# print(sys.path)
+# print(os.name)
+# print(platform.system())
+
+# print(math.ceil(math.sqrt(4)))
+# print(ceil(s(25)))
+
+
+# Власні модулі
+
+# name = "Inna"
+
+# def hi():
+#   print("Hello", name)
+  
+# def add (a, b, c):
+#   return a + b + c
+
+
+# Тепер можемо імпортувати наш файл або частинку, і використовувати дані з нього import modules
+
