3514a8f35bd3351b6393348c3f42555b67b6d8fd
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 19:34:23 +0200

    tuple

---
+++ tuple.py
@@ -0,0 +1,19 @@
+#кортежі () ств. за допомогою круглих дужок
+
+data = (1, 2, 3, True, "text", [1, 2, 3])
+# print(data)
+# print(data[0])
+# print(data.count(3))
+#data[0] = 5 Не можна додавати нові значення в кортежі та видаляти старі
+
+
+#Кортеж містить 2 і більше елементів
+# info = (4,5)
+# print(info)
+
+# for el in data:
+#   print(el)
+
+nums = [5, 6]
+data = tuple(nums)
+print(data)
