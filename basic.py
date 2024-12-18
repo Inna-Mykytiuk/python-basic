754f47e561f2225fbf9d37480c754a22cae0640d
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 17:23:53 +0200

    умовні конструкціїconditional constructions

---
+++ basic.py
@@ -0,0 +1,28 @@
+# num = int(input("Enter a number: "))
+
+
+# num != 55 num == 55
+# if num == 55:
+#   print("Number is more or equal to 55")
+#   if num == 100:
+#     print("Number is equal to 100")
+# elif num == 40:
+#   print("Number is equal to 40")
+# else:
+#   print("Number is less than 55")
+    
+# isHappy = True
+# if not isHappy:
+#   print("You are happy")
+  
+# print("More text")
+
+# логічні оператори || - or, && - and, ! - not записуються не символом а словом
+
+# умовні конструкції if else elif(else if)
+
+# data = "Info"
+
+# correct = True if data == "Info" else False
+
+# print(correct)
