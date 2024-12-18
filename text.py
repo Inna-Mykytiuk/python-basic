73605f7f8b08a1a2948a20d888235132e7c54c14
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 19:23:32 +0200

    work with str and elements

---
+++ text.py
@@ -0,0 +1,34 @@
+# метод list схожий на метод split для розділення рядків
+
+# word = list("inna")
+# word[0] = "I" 
+# word.append("!")
+# print(word)
+
+# word = "".join(word)
+
+# print(word.upper())
+# print(word.islower())
+
+# text = "football,bascketball,volleyball"
+# hobbies = text.split(",")
+
+# for i in range(0, len(hobbies)):
+#   hobbies[i] = hobbies[i].capitalize()
+  
+# result = ",".join(hobbies)
+  
+# print(result)
+
+#індекси та зрізи
+
+# lis = [5, 1, 11, 3, False, True, 7]
+# print(lis[1:4])
+# #вивід від індексу до кінця масиву
+# print(lis[2:])
+# #вивід до індексу елементу
+# print(lis[:2])
+# #вивід елементів з кроком 2
+# print(lis[0::2])
+# #вивід у зворотному порядку
+# print(lis[::-1])
