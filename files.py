b21fa6c7b5dbe4702648a961d3dd3e631fea859b
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 20:46:52 +0200

    work with files

---
+++ files.py
@@ -0,0 +1,31 @@
+# Робота з файлами
+# "w" - відкриття існуючого файлу 
+# "w+" - автоматичне створення файлу, якщо він не існує
+# "a" - додавання тексту в кінець файлу
+# "r" - читання файлу
+# Обов'язково потрібно закрити файл за допомогою file.close()
+
+
+# file = open('data/myfile.txt', 'a')
+# file.write('Hello, world!\n')
+# file.write('Hello, Inna!')
+
+# file.close()
+
+
+# data = input("Hobby: ")
+
+# file = open('data/myfile.txt', 'a')
+# file.write(data + "\n")
+
+# file.close()
+
+# file = open('data/myfile.txt', 'r')
+# # print(file.read())
+# #Зчитує кількість вказаних символів
+# # print(file.read(10))
+
+# for line in file:
+#   print(line, end="")
+
+# file.close()
