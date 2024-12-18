23da68ac9297d04774668412c34d3f29705bc28e
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 18:20:59 +0200

    ccycles

---
+++ cycles.py
@@ -0,0 +1,47 @@
+# for i in range(5, 16, 2):
+#   print("El: ", i)
+
+# print("\n\n")
+  
+# word = "Some text"
+# for i in word:
+#   if i == "o":
+#     print("Word contains: ", i)
+    
+
+# while
+# i = 0
+# while i <= 5:
+#   print("El: ", i)
+#   i += 1
+
+#task
+
+# work = True
+
+# while work:
+#   user_input = input("Enter word stop: ")
+#   if user_input == "stop":
+#     work = False
+    
+# print("Done")
+
+# for i in range(1, 11):
+#   if i % 2 == 0:
+#     continue
+  
+#   if i == 7:
+#     break
+  
+#   print("El: ", i)
+
+
+#else in cycles
+
+# for i in "Hello world":
+#   if i =="l":
+#     print("Done")
+#     break
+
+# else:
+#   print("Not found")
