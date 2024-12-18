1f26b4abd87cb0acbebb50d58b9533e7d04ee0b3
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 20:34:27 +0200

    def, lambda functions

---
+++ functions.py
@@ -0,0 +1,67 @@
+# Функції створюються через def
+
+# def hello():
+#   print("Hello", end="")
+#   print("!")
+  
+
+# hello()
+
+# def testFunc(word):
+#   print(word, end="")
+#   # print("!")
+  
+
+# # testFunc("Hello")
+# # word = "home"
+
+# # testFunc(word)
+# # testFunc("Hi")
+
+# def sum(a, b):
+#   res = a + b
+#   # testFunc(res)
+#   return res
+  
+# # sum(5,6)
+# # sum("Hi", " world")
+
+# res1 = sum(5,6)
+# res2 = sum("Hi", " world")
+
+# print(res1)
+
+
+# Task
+
+# def minimal(array):
+#   minimum = array[0]
+
+#   for num in array:
+#     if num < minimum:
+#       minimum = num
+  
+#   return minimum
+
+
+# nums = [1, 2, 7, 8, 9, 10, -3, 0, 26]
+# res1 = minimal(nums)
+
+# nums2 = [1, 2, 7, 8, 9, 10, -8, 0, 26]
+# res2 = minimal(nums2)
+
+# if res1 < res2:
+#   print(res1)
+# else:
+#   print(res2)
+
+# Lambda функції (анонімні функції)
+
+# func = lambda x, y: x + y
+# res = func(5, 6)
+# print(res)
+
+
+
+
+
