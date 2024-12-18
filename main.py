6cd1f0340f6215ee183aa9d2a30eed5d8b6b59b9
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 13:53:56 +0200

    basic concepts

---
+++ main.py
@@ -0,0 +1,29 @@
+# #My comments
+# print('Hello Python!', 10, 3.5, sep=", ", end='!\n')
+# print('\n\tHello')
+# print('Inna\'s comment\\')
+
+# #Math
+# print(5+10)
+
+# #variables
+# number = 5
+# # del number
+# print(number)
+# number = 7  
+# print("Value of number: ", number)
+
+# num = 444
+# word = "Hello"
+# boolean = True
+# print(num, word, boolean)
+
+# #int (тип даних для чисел), str (строка), float (дійсні числа), bool (логічні значення)
+
+# # a = input("Enter a number 1: ")
+# a = int(input("Enter a number 1: "))
+# b = input("Enter a number 2: ")
+
+# a += 5
+# # a /= 5 - ділення
+# print(a+ int(b))
