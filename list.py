bbff8e9e108426c5c69a3d9557ba466d4242a54c
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-18 19:06:44 +0200

    lists/arrays

---
+++ list.py
@@ -0,0 +1,69 @@
+# data list/arrays
+
+# nums = [5, True, 7.47, "6", 10]
+# firstEl = nums[0]
+
+# print(firstEl)
+
+# nums.append(45)
+# print(nums)
+# nums.insert(1, False)
+# print(nums)
+
+# nums2 = [5, True, 7.47, [False, "text", 10]]
+# print(nums2[-2])
+
+#False=0
+#True=1
+
+
+#list2
+# nums3 = [5, 1, 13, 2, False, True, 2]
+
+# nums3.sort()
+# print(nums3)
+
+# nums3.reverse()
+# print(nums3)
+
+# nums3.remove(False)
+# print(nums3)
+
+# nums3.pop()
+# print(nums3)
+
+# nums3.clear()
+# print(nums3)
+
+# print(nums3.index(2))  
+# print(nums3.count(2))
+# print(len(nums3))
+
+
+#task3
+
+# nums = [5, 1, 13, 2, False, True, 2]
+# nums2 = [5, 1, 13, 2, False, True, 2]
+
+# print(nums == nums2)
+
+# nums = [5, 1, 11, 3, 7]
+
+# for el in nums:
+#   res = el * 2
+#   print(res)
+
+
+#practice
+
+# userCount = int(input("Enter hobby number: "))
+
+# i = 0
+# hobby = []
+
+# while i < userCount:
+#   text = "Enter hobby: " + str(i + 1) + ": "
+#   hobby.append(input(text))
+  
+#   i += 1
+# print(hobby)
