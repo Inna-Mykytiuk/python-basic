f92c7eb2dd86804260c4a4fd387ceb8c84a48f39
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-19 11:28:56 +0200

    Decorators

---
+++ decorators.py
@@ -0,0 +1,16 @@
+# Декоратори функцій
+# import webbrowser
+
+# def validator(func):
+#   def wrapper(url):
+#     if "." in url:
+#       func(url)
+#     else:
+#       print("Invalid url")
+#   return wrapper
+
+# @validator
+# def open_url(url):
+#   webbrowser.open(url)
+  
+# open_url("https://google.com")
