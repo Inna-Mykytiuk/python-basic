1867060e3487111756c39a1ced343f8de298d560
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-19 10:56:19 +0200

    Constructors, method overriding

---
+++ constructors.py
@@ -0,0 +1,24 @@
+# Конструктори, перевизначення методів
+
+# class Dog:
+#   name = None 
+#   age = None
+#   isHappy = None
+  
+#   def __init__(self, name="Guffy", age=1, isHappy=True):
+#     self.set_data(name, age, isHappy)
+#     self.get_data()
+  
+#   def set_data(self, name, age=1, isHappy=True):
+#     self.name = name
+#     self.age = age
+#     self.isHappy = isHappy
+    
+#   def get_data(self):
+#     print(self.name, "Age:", self.age, ". Is happy:", self.isHappy)
+  
+  
+# # dog1 = Dog( "Scooby" )
+# dog1 = Dog( age=3 )
+# # dog1.set_data("Scooby", 5, True)
+# dog2 = Dog("Max", 3, False)
